c526f7287b6423f10371bf1c689b1de443a6045c
Ole Streicher <olebole@debian.org>
2025-11-19 08:50:15 +0000

    iraf-fitsutil: add 2024.07.06 bottle.

--- Formula/iraf-fitsutil.rb
+++ Formula/iraf-fitsutil.rb
@@ -4,6 +4,11 @@ class IrafFitsutil < Formula
   url "https://github.com/iraf-community/iraf-fitsutil/archive/refs/tags/v2024.07.06.tar.gz"
   sha256 "6f58744669a84e021a3c931f448645053d2ceb65213412f0688e11f48e2b5769"
 
+  bottle do
+    root_url "https://github.com/iraf-community/homebrew-tap/releases/download/iraf-fitsutil-2024.07.06"
+    sha256 cellar: :any, arm64_tahoe: "2987d9a8d4f5faa9343c84c1f226a0c83351e47158cadeabed56ab9d1ebe5fb7"
+  end
+
   depends_on "cfitsio"
   depends_on "iraf"
 
