68a680868c20b6e91b39c84cf7323f4c260896dc
Ole Streicher <olebole@debian.org>
2025-11-20 20:27:01 +0000

    iraf-rvsao: add 2.8.5 bottle.

--- Formula/iraf-rvsao.rb
+++ Formula/iraf-rvsao.rb
@@ -4,6 +4,12 @@ class IrafRvsao < Formula
   url "http://tdc-www.harvard.edu/iraf/rvsao/rvsao-2.8.5.tar.gz"
   sha256 "0ccd06a4ff4ba5a344787406145f1348ba0cfbaf7771e51e0356ce8e93979b1a"
 
+  bottle do
+    root_url "https://github.com/iraf-community/homebrew-tap/releases/download/iraf-rvsao-2.8.5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "3019764443f04b4656c8f0e0042419b43cfca20785e883b86e03b29d5a4ac8e1"
+    sha256 cellar: :any_skip_relocation, sequoia:     "4406414d0c68acaa902d8cf3606b5af6be1f5c9659be23b8e39d3c986a566c42"
+  end
+
   depends_on "iraf"
 
   patch :DATA
