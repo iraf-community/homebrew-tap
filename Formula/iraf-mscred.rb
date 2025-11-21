521127159dc2ac3ad629770e684f967d96f52f2b
Ole Streicher <olebole@debian.org>
2025-11-21 08:51:53 +0000

    iraf-mscred: add 5.05+20250915 bottle.

--- Formula/iraf-mscred.rb
+++ Formula/iraf-mscred.rb
@@ -5,6 +5,12 @@ class IrafMscred < Formula
   version "5.05+20250915"
   sha256 "1e447c7e0cab3b41be4dbf3e390c7f94095d7b3dd1390269f1c80733d8512c7d"
 
+  bottle do
+    root_url "https://github.com/iraf-community/homebrew-tap/releases/download/iraf-mscred-5.05+20250915"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "ae6e44f756339b167f7ae7c86a1e4eb75a6d1a6b740832176ea5c59b8cfb4fc0"
+    sha256 cellar: :any_skip_relocation, sequoia:     "0f6589435bd9560fec0662d08b60032485fb72b4f3fd7550f6886591b480f5c6"
+  end
+
   depends_on "iraf"
 
   def install
