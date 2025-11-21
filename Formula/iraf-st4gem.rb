1cd82bd13f5ff16eeebe21cf82a28ce32e19c8d5
Ole Streicher <olebole@debian.org>
2025-11-21 23:47:38 +0000

    iraf-st4gem: add 1.2.1 bottle.

--- Formula/iraf-st4gem.rb
+++ Formula/iraf-st4gem.rb
@@ -4,6 +4,12 @@ class IrafSt4gem < Formula
   url "https://gitlab.com/nsf-noirlab/csdc/usngo/iraf/st4gem.git",
       tag: "v1.2.1", revision: "e61f8c2f69265feb849fe065d409cf41bd635473"
 
+  bottle do
+    root_url "https://github.com/iraf-community/homebrew-tap/releases/download/iraf-st4gem-1.2.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "eb775d4d152462608e6516951e0dd9ad22b417127db978473e75f78bc3db4202"
+    sha256 cellar: :any_skip_relocation, sequoia:     "42f52038be1620326303a04e91cc828dd687143dd096f46bb05ba51cb09dcd1c"
+  end
+
   depends_on "iraf"
 
   def install
