e9f9eb199896fa778ab8a13d9a49283024b46da7
Ole Streicher <olebole@debian.org>
2025-11-28 10:02:05 +0000

    iraf: add 2.18.1 bottle.

--- Formula/iraf.rb
+++ Formula/iraf.rb
@@ -4,6 +4,15 @@ class Iraf < Formula
   url "https://github.com/iraf-community/iraf/archive/refs/tags/v2.18.1.tar.gz"
   sha256 "d4e0859088459622625d27b5c025524dc70fbf334e8df5e59dd32b65630e7981"
 
+  bottle do
+    root_url "https://github.com/iraf-community/homebrew-tap/releases/download/iraf-2.18.1"
+    rebuild 5
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f3d9bbdbdf55d3eb3930f8cea896bbe6384c17f80a0bf1282f3bca49e64ad433"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6f10d3e3799931adfae4bdb35839f98d30340f45135889194456d27ef6d3cd55"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c397d9d4e677b1a4c19d404a9c58742ee8680e9bf1cfb078688f7f61092ca07b"
+    sha256 cellar: :any_skip_relocation, sequoia:       "1b362b85e25fff0d55634f1ffdf00d3583596eeb8bd521d962ae0dd3b8199a95"
+  end
+
   def install
     system "make", "IRAFARCH="
     system "make", "install", "DESTDIR=build", "prefix=/usr"
