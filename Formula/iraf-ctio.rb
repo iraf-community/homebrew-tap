d43c73a3562909e90387eb25edfd170d4cb3f318
Ole Streicher <olebole@debian.org>
2025-12-04 12:19:34 +0000

    iraf-ctio: update 2023.11.12 bottle.

--- Formula/iraf-ctio.rb
+++ Formula/iraf-ctio.rb
@@ -6,11 +6,11 @@ class IrafCtio < Formula
   sha256 "987dbc80e2df98624b192a03d7829f717f51aa70580ea7a8ed3ef24f09aa2708"
 
   bottle do
-    root_url "https://github.com/iraf-community/homebrew-tap/releases/download/iraf-ctio-202.11.12"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f4e1fdd98549f717ead3bd7ae25e33e06b21696eafd9983c18af6fb8841e07c2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "24a8e7a4471cea607ed47b596e77b66ddbb63e54fc22fc40636395dd2c11cbff"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4a7d4ee76f88c6ec75df07de2eb2607fa65b294add7ee122fd51d75a7edd6b05"
-    sha256 cellar: :any_skip_relocation, sequoia:       "02804f6c91ecaf7bf50b6a1c7efcc74c0530246f2d06db7607c0333f4faa8ca1"
+    root_url "https://github.com/iraf-community/homebrew-tap/releases/download/iraf-ctio-2023.11.12"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "800fab8ca91a74f94f043c2cc48ce48f7249db2e4fb9538a8f7d715d7d7c1f07"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8f504118ce58d65fb03055b37bc48a11875db5b1dba3f61dfd1d2724f6b6e486"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "79a6f51d1bae7c4dab156a8bc994c8cc581e7f95518aa6063f92160a12a11fd7"
+    sha256 cellar: :any_skip_relocation, sequoia:       "0d4d6245f8b22ebf916e9ba20b5d029900fa7ab8f0b9bc7c06e64b229a7f0a39"
   end
 
   IRAF_PACKAGE = "ctio".freeze
