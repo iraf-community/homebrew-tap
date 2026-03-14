99d488a70a54d0a2cb5c6002d075a3d0adf224dc
Ole Streicher <olebole@debian.org>
2026-03-14 16:24:53 +0000

    x11iraf: add 2.2 bottle.

--- Formula/x11iraf.rb
+++ Formula/x11iraf.rb
@@ -4,6 +4,14 @@ class X11iraf < Formula
   url "https://github.com/iraf-community/x11iraf/archive/refs/tags/v2.2.tar.gz"
   sha256 "f5c6e0b56a1f68e8c15a766d8521ec47176c4f4cb3d38e48f142dd0261746f91"
 
+  bottle do
+    root_url "https://github.com/iraf-community/homebrew-tap/releases/download/x11iraf-2.2"
+    rebuild 6
+    sha256 cellar: :any, arm64_tahoe:   "280dbfe0fc2dd5e8355cffdf709fad0e703fecc905fd6e32c2f7e7ed93996c29"
+    sha256 cellar: :any, arm64_sequoia: "e4b1f8ef932393f7de939281695a53dd5ca8a8bdeee885a0fea3be5a50a47f96"
+    sha256 cellar: :any, arm64_sonoma:  "d5edc6e1e8ca9e746997ea3551810fb7eef66eb4c785bbb81be958cf30aecabc"
+  end
+
   depends_on "iraf" => :build
   depends_on "libx11"
   depends_on "libxaw"
