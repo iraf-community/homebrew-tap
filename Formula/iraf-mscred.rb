747f0add0dc3ad356ab09c883ee4d25e5d026d1d
Ole Streicher <olebole@debian.org>
2026-03-14 16:24:52 +0000

    iraf-mscred: add 5.05+20250915 bottle.

--- Formula/iraf-mscred.rb
+++ Formula/iraf-mscred.rb
@@ -5,6 +5,16 @@ class IrafMscred < Formula
   version "5.05+20250915"
   sha256 "1e447c7e0cab3b41be4dbf3e390c7f94095d7b3dd1390269f1c80733d8512c7d"
 
+  bottle do
+    root_url "https://github.com/iraf-community/homebrew-tap/releases/download/iraf-mscred-5.05+20250915"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2b581736cbc5ba889d5c950b1e420976d396a159ae1bc67ad1b5c81f007c42b1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a04c9826c4d2aef9b9759af36aa5c43ad124213d8b72178f318a28d8dbfa7ac3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bb39f845991670fdf067a3e2162c32b455202c9e0ca9fd948ba353046093c174"
+    sha256 cellar: :any_skip_relocation, tahoe:         "1858329d4493944072398d3f64a698d22667c26e0f923afe27bd7ec286efac05"
+    sha256 cellar: :any_skip_relocation, sequoia:       "069f768b2e6b3d980dec958d9565a7ea9e1e847caa03fcc45180ce1c9def15a1"
+  end
+
   IRAF_PACKAGE = "mscred".freeze
 
   depends_on "iraf"
