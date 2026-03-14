84cfadbac97acdfcb32070e8797d6b67e7a81679
Ole Streicher <olebole@debian.org>
2026-03-14 16:24:52 +0000

    iraf-rvsao: add 2.8.5 bottle.

--- Formula/iraf-rvsao.rb
+++ Formula/iraf-rvsao.rb
@@ -4,6 +4,16 @@ class IrafRvsao < Formula
   url "http://tdc-www.harvard.edu/iraf/rvsao/rvsao-2.8.5.tar.gz"
   sha256 "0ccd06a4ff4ba5a344787406145f1348ba0cfbaf7771e51e0356ce8e93979b1a"
 
+  bottle do
+    root_url "https://github.com/iraf-community/homebrew-tap/releases/download/iraf-rvsao-2.8.5"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1c10d25b3bdd057d81479934f9cda8637b7d81f66c246e8b3cc8c1453ad30037"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "977fe98684dd040f4d06bd9f4b7f58e85160f05383532cb3fa532f1893968e79"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "406c18e74a2756eb55c20c4fbdee98cba5536c5e61b922b35b94ca8055fc00e4"
+    sha256 cellar: :any_skip_relocation, tahoe:         "de472a3862315a024e77c94f99adcb9f2299b92cc9184e8b6f338d3d9faec40a"
+    sha256 cellar: :any_skip_relocation, sequoia:       "9fb241a3c644053b05ff9c2778ee487031b06bab3e9991eb534a50dd4ee57221"
+  end
+
   IRAF_PACKAGE = "rvsao".freeze
 
   depends_on "iraf"
